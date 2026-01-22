5e51548a0f8fd83ae8d2d09d057e0f5574379e97
amanpreetchatha <amanpreetchatha99@gmail.com>
2026-01-22 20:15:17 +0530

    added weight form

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,8 @@
 import useUserContext from "~/context";
 import type { Route } from "./+types/home";
 import Login from "./login";
+import supabase from "../supabase-client";
+import { useState } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,11 +13,35 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   const user=useUserContext();
-  
+  const [enteredWeight,setEnteredWeight]=useState("");
+  const [message,setMessage]=useState("");
+  async function setWeight(){
+    try{
+      const {data, error} = await supabase
+      .from("metrics")
+      .insert({user_id: user?.id,
+        weight: enteredWeight,
+        created_at: new Date(),
+      })
+      if(error)
+        setMessage(error.message);
+      setMessage("Saved Successfully!");
+      setEnteredWeight("");
+    }catch(error: any){
+      setMessage(error.message);
+    }
+  }
   //issue: flash of login component before user home component.
   return user===undefined ? <Login/> : (
-    <div>
-      <h1>This is a dashboard for {user.email}</h1>
+    <div className="weight-form">
+      <form>
+        <div>
+          <label>Weight</label>
+          <input placeholder="Enter weight" value={enteredWeight} ></input>
+          <button onClick={setWeight}>Save</button>
+          <p>{message}</p>
+        </div>
+      </form>
     </div>
   );
 }
